f295410bd4e9de5afdcc6fc1bc510e6e6be676d7
Navtej Singh <navtej@uchicago.edu>
2020-04-28 21:57:02 -0400

    Update Add Formspace Support

--- form.py
+++ form.py
@@ -11,6 +11,7 @@ Navtej Singh <singhnav@umich.edu>
 from __future__ import print_function
 
 import requests
+from   time     import sleep
 
 # Parameters #
 
@@ -22,7 +23,7 @@ ps = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
 
 # Utilities
 
-def get_form_params(verbose=False,debug=True,**parameters):
+def get_form_params(space=False,verbose=False,debug=True,**parameters):
     req = 'https://www.lmfdb.org/api/mf_newforms?_format=json&' + '&'.join('{}={}'.format(k,
         v) for k,v in parameters.items())
     if verbose: print(req)
@@ -31,21 +32,23 @@ def get_form_params(verbose=False,debug=True,**parameters):
         json_data = response.json()
         if 'data' not in json_data or len(json_data['data']) == 0:
             raise Exception('No such form found')
-        return json_data['data'][0]
+        return json_data['data'][0] if not space else json_data['data']
     else:
         raise Exception('Request failed')
 
-def example_form():
+def test_example_form():
     form = Form(weight=1,level=(3**3)*(7**2),artin_degree=6)
-    print('p,a_p')
-    for p in ps:
-        print('{},{}'.format(p, form.trace(p)))
+    assert (form.traces[:10] == [1, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+
+def test_example_formspace():
+    formspace = Formspace(weight=1,level=(3**3)*(7**2))
+    assert (formspace.form113746_traces[:10] == formspace.findone(artin_field_label='3.1.1323.1')['traces'][:10])
 
 # Classes #
 
 class Form(object):
-    def __init__(self,**params):
-        print('Instantiated Form of {}'.format(', '.join(['{} {}'.format(k.capitalize(), 
+    def __init__(self,verbose=False,**params):
+        if verbose: print('Instantiated Form of {}'.format(', '.join(['{} {}'.format(k.capitalize(), 
             v) for k, v in sorted(params.items())])))
         params = get_form_params(**params)
         for k,v in params.items():
@@ -56,3 +59,33 @@ class Form(object):
             return self.traces[n-1]
         except Exception as e:
             raise NotImplementedError
+
+class Formspace(object):
+    def __init__(self,verbose=False,**params):
+        self.forms = get_form_params(space=True,**params)
+        form_methods = []
+        for form in self.forms:
+            for k,v in form.items():
+                setattr(self,'form{}_{}'.format(form['id'], k), v)
+                form_methods.append('form{}_{}'.format(form['id'], k))
+        self.form_methods = form_methods
+        self.number_of_forms = len([attr for attr in dir(self) if 'form' in attr])
+        if verbose: print('Found {} Forms of {}'.format(self.number_of_forms, 
+            ', '.join(['{} {}'.format(k.capitalize(), 
+            v) for k, v in sorted(params.items())])))
+
+    def find(self, **kwargs):
+        candidates = [form for form in self.forms if all([form[k] == v for k, v in kwargs.items()])]
+        if len(candidates) == 0:
+            raise Exception('None found')
+        return candidates
+
+    def findone(self, **kwargs):
+        candidates = [form for form in self.forms if all([form.get(k) == v for k, v in kwargs.items()])]
+        if len(candidates) == 0:
+            raise Exception('None found')
+        return candidates[0]
+
+if __name__ == '__main__':
+    test_example_form()
+    test_example_formspace()
